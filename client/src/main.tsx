da4fc62771a61cf4435939c75721fd0e111b9c58
rushibamb <rushibamb@gmail.com>
2025-12-25 16:07:48 +0530

    fix: add start script for render backend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,8 @@
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
+  console.log("React app started");
+
 
   createRoot(document.getElementById("root")!).render(<App />);
   
